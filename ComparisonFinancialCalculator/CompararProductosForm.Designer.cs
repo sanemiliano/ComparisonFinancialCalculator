0f9da132facfc965ad988b80b5db28f879350d33
sanemiliano <cajademensajes@outlook.es>
2018-11-27 16:17:15 -0600

    CompararProductosForm graphics ready.

--- ComparisonFinancialCalculator/CompararProductosForm.Designer.cs
+++ ComparisonFinancialCalculator/CompararProductosForm.Designer.cs
@@ -29,6 +29,29 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trema1 = new System.Windows.Forms.TextBox();
+            this.inicial1 = new System.Windows.Forms.TextBox();
+            this.mantenimiento1 = new System.Windows.Forms.TextBox();
+            this.nper1 = new System.Windows.Forms.TextBox();
+            this.recuperacion1 = new System.Windows.Forms.TextBox();
+            this.comparar_button = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.recuperacion2 = new System.Windows.Forms.TextBox();
+            this.nper2 = new System.Windows.Forms.TextBox();
+            this.mantenimiento2 = new System.Windows.Forms.TextBox();
+            this.inicial2 = new System.Windows.Forms.TextBox();
+            this.trema2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -41,20 +64,254 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TREMA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Número de periodos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Costo de mantenimiento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Costo inicial";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Valor de recuperación";
+            // 
+            // trema1
+            // 
+            this.trema1.Location = new System.Drawing.Point(247, 65);
+            this.trema1.Name = "trema1";
+            this.trema1.Size = new System.Drawing.Size(100, 22);
+            this.trema1.TabIndex = 6;
+            // 
+            // inicial1
+            // 
+            this.inicial1.Location = new System.Drawing.Point(247, 185);
+            this.inicial1.Name = "inicial1";
+            this.inicial1.Size = new System.Drawing.Size(100, 22);
+            this.inicial1.TabIndex = 7;
+            // 
+            // mantenimiento1
+            // 
+            this.mantenimiento1.Location = new System.Drawing.Point(247, 136);
+            this.mantenimiento1.Name = "mantenimiento1";
+            this.mantenimiento1.Size = new System.Drawing.Size(100, 22);
+            this.mantenimiento1.TabIndex = 8;
+            // 
+            // nper1
+            // 
+            this.nper1.Location = new System.Drawing.Point(247, 100);
+            this.nper1.Name = "nper1";
+            this.nper1.Size = new System.Drawing.Size(100, 22);
+            this.nper1.TabIndex = 9;
+            // 
+            // recuperacion1
+            // 
+            this.recuperacion1.Location = new System.Drawing.Point(247, 231);
+            this.recuperacion1.Name = "recuperacion1";
+            this.recuperacion1.Size = new System.Drawing.Size(100, 22);
+            this.recuperacion1.TabIndex = 10;
+            // 
+            // comparar_button
+            // 
+            this.comparar_button.Location = new System.Drawing.Point(362, 358);
+            this.comparar_button.Name = "comparar_button";
+            this.comparar_button.Size = new System.Drawing.Size(94, 41);
+            this.comparar_button.TabIndex = 21;
+            this.comparar_button.Text = "Comparar";
+            this.comparar_button.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(164, 24);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(77, 17);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Producto 1";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(539, 24);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(77, 17);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Producto 2";
+            // 
+            // recuperacion2
+            // 
+            this.recuperacion2.Location = new System.Drawing.Point(627, 226);
+            this.recuperacion2.Name = "recuperacion2";
+            this.recuperacion2.Size = new System.Drawing.Size(100, 22);
+            this.recuperacion2.TabIndex = 33;
+            // 
+            // nper2
+            // 
+            this.nper2.Location = new System.Drawing.Point(627, 95);
+            this.nper2.Name = "nper2";
+            this.nper2.Size = new System.Drawing.Size(100, 22);
+            this.nper2.TabIndex = 32;
+            // 
+            // mantenimiento2
+            // 
+            this.mantenimiento2.Location = new System.Drawing.Point(627, 131);
+            this.mantenimiento2.Name = "mantenimiento2";
+            this.mantenimiento2.Size = new System.Drawing.Size(100, 22);
+            this.mantenimiento2.TabIndex = 31;
+            // 
+            // inicial2
+            // 
+            this.inicial2.Location = new System.Drawing.Point(627, 180);
+            this.inicial2.Name = "inicial2";
+            this.inicial2.Size = new System.Drawing.Size(100, 22);
+            this.inicial2.TabIndex = 30;
+            // 
+            // trema2
+            // 
+            this.trema2.Location = new System.Drawing.Point(627, 60);
+            this.trema2.Name = "trema2";
+            this.trema2.Size = new System.Drawing.Size(100, 22);
+            this.trema2.TabIndex = 29;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(448, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 17);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Valor de recuperación";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(448, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 17);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Costo inicial";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(448, 141);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(160, 17);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Costo de mantenimiento";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(448, 100);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(137, 17);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Número de periodos";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(448, 65);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 17);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "TREMA";
+            // 
             // CompararProductosForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.recuperacion2);
+            this.Controls.Add(this.nper2);
+            this.Controls.Add(this.mantenimiento2);
+            this.Controls.Add(this.inicial2);
+            this.Controls.Add(this.trema2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.comparar_button);
+            this.Controls.Add(this.recuperacion1);
+            this.Controls.Add(this.nper1);
+            this.Controls.Add(this.mantenimiento1);
+            this.Controls.Add(this.inicial1);
+            this.Controls.Add(this.trema1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "CompararProductosForm";
             this.Text = "CompararProductosForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox trema1;
+        private System.Windows.Forms.TextBox inicial1;
+        private System.Windows.Forms.TextBox mantenimiento1;
+        private System.Windows.Forms.TextBox nper1;
+        private System.Windows.Forms.TextBox recuperacion1;
+        private System.Windows.Forms.Button comparar_button;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox recuperacion2;
+        private System.Windows.Forms.TextBox nper2;
+        private System.Windows.Forms.TextBox mantenimiento2;
+        private System.Windows.Forms.TextBox inicial2;
+        private System.Windows.Forms.TextBox trema2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
